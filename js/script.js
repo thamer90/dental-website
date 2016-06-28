0c136dc78860f53a30a686c68425a398bb949513
thamer90 <thameralmerry@gmail.com>
2016-06-29 02:14:39 +1000

    Created header, nav and banner. Added js and fonts folder.

---
+++ js/script.js
@@ -0,0 +1,12 @@
+/*
+Script for Website
+ Authors: Thamer Al-Merry
+ Date: 28 June 2016
+*/
+
+// On document ready
+$(document).ready(function() {
+  $('.js-mobile').click(function () {
+      $('nav').toggleClass('active');
+   });
+});
